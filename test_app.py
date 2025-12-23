31658ac1021147e289ce31577ebf211e0700895a
Vaishnavi-Github-collab <my.vaishnavipal@gmail.com>
2025-12-23 22:57:06 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,12 +1,13 @@
 import requests
 
-BASE_URL = "http://localhost:5000"
+BASE_URL = "http://127.0.0.1:5000"
 
-def test_health():
-    r = requests.get(f"{BASE_URL}/health")
-    assert r.status_code == 200
-    assert r.json()["status"] == "OK"
+def test_health_endpoint():
+    response = requests.get(f"{BASE_URL}/health")
+    assert response.status_code == 200
+    assert response.json()["status"] == "OK"
 
-def test_add():
-    r = requests.get(f"{BASE_URL}/add/2/3")
-    assert r.json()["result"] == 5
+def test_add_endpoint():
+    response = requests.get(f"{BASE_URL}/add/2/3")
+    assert response.status_code == 200
+    assert response.json()["result"] == 5
